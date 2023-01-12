a88d6d82df2057cbe0607955306efc38c9250d68
windupratama <104411387+windupratama@users.noreply.github.com>
2023-01-12 23:16:32 +0700

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,21 +1,30 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Code {
+public class Testfield {
     public static void main(String[] args) {
         //initialize
         int minValue, maxValue, randomValue, userGuess;
 
-        Scanner userInput = new Scanner(System.in);         //calling scanner class for user input
-        Random random = new Random();                       //calling random class for randoming number purpose
+        Scanner userInput = new Scanner(System.in);   //calling scanner class for user input
+        Random random = new Random();   //calling random class for randoming number purpose
+        
+        //getting random number guessing range and random answer of the guessing number
+        minValue = 0; maxValue = 100;
+        randomValue = random.nextInt(minValue, maxValue);
 
-        minValue = 0; maxValue = 10;                        //setting the min and max value of generated random number
+        minValue = randomValue;
+
+        maxValue = 100;
+        randomValue = random.nextInt(minValue, maxValue);
+        
+        maxValue = randomValue;   //setting the min and max value of generated random number
         randomValue = random.nextInt(minValue, maxValue);   //getting a random number from 0 to 10
 
         System.out.println("Welcome to Cosmic' Number Guessing!");
-        System.out.println("Please pick a number from range 0 to 10");
+        System.out.printf("Please pick a number from range %d to %d\n", minValue, maxValue);
         System.out.print("Number guess : ");
-        userGuess = userInput.nextInt();                    //getting input from user
+        userGuess = userInput.nextInt();    //getting input from user
 
         //checking the user answer
         if(userGuess == randomValue){
